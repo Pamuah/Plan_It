63b11db1ac8d485d25c5e07bc711a4e546ae4fd8
Patrick Amuah <patrickamuah38@gmail.com>
2025-03-25 12:56:07 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-primary">Plan It and execute</h1>
+    </div>
+  );
+};
+
+export default App;
