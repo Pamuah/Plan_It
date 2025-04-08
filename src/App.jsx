651d014007600d8b4dbae1064b1c09047a2e5009
Patrick Amuah <patrickamuah38@gmail.com>
2025-04-08 10:14:40 +0000

    added routes to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import CustomButton from "./components/CustomButton";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import SignUp from "./pages/signUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl text-primary">Plan It and execute</h1>
-
-      <CustomButton
-        title="Login"
-        backgroundColor="#007AFF"
-        onPress={() => console.log("Login button clicked")}
-        trailingIcon={faUser}
-      />
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<SignUp />} />
+        <Route path="/signUp" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/pages/signUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div className="text-3xl text-amber-400">Hello I'm new here</div>;
+};
+
+export default SignUp;
