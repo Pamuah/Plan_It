e9bffc0e9df6e8402616b88bc35aa0d09d3aeada
KwamenaJR <KwamenaJR@example.com>
2025-05-05 10:01:26 +0000

    message

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import SignUp from "./pages/signUp";
 // import SignIn from "./pages/signIn";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route index element={<SignUp />} />
+        <Route index element={<LandingPage />} />
         <Route path="/signUp" element={<SignUp />} />
+        <Route path="/LandingPage" element={<LandingPage />} />
         {/* <Route path="/signIn" element={<SignIn />} /> */}
       </Routes>
     </Router>
