32c6b07a99327bbeaa8597dd242fa95f65289ef6
Ernest Amoah <108088829+alphakobb@users.noreply.github.com>
2025-07-26 08:10:49 +0000

    added a page for vendor

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import LandingPage from "./LandingPage";
 import HomePage from "./pages/homePage";
+import Vendor from "./pages/Vendor";
 
 const App = () => {
   return (
@@ -16,8 +17,8 @@ const App = () => {
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/reset" element={<ForgotPassword />} />
-
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/vendors" element={<Vendor />} />
 
         <Route path="/LandingPage" element={<LandingPage />} />
         <Route path="/homePage" element={<HomePage />} />
